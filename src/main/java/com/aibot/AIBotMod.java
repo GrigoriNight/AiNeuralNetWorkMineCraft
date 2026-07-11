bdf9b139cdb94c0088f5f973abad12e77df4bdc3
GrigoriNight <79440520+GrigoriNight@users.noreply.github.com>
2026-07-11 02:46:46 -0500

    Bump version to 1.1.0

--- src/main/java/com/aibot/AIBotMod.java
+++ src/main/java/com/aibot/AIBotMod.java
@@ -35,7 +35,7 @@ public class AIBotMod {
     // below instead, which is safe to change freely.
     public static final String MODID = "aibot";
     public static final String NAME = "AiNeuralNetWorkMineCraft";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.1.0";
 
     @Mod.Instance(MODID)
     public static AIBotMod instance;
